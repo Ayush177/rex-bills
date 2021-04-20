04dec0d54b3004a16d8c0c7ef9dd30dbcb95eae5
LazyBST <ayusharora1778@gmail.com>
2021-04-20 21:37:39 +0530

    Added search by date feature

--- src/pages/Bills.js
+++ src/pages/Bills.js
@@ -1,20 +1,37 @@
 import { navigate } from "@reach/router";
 import { useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Form } from "react-bootstrap";
 import Axios from "../utils/Axios";
 import Analytics from "../components/Analytics";
 
 const Bills = () => {
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+  today = yyyy + "-" + mm + "-" + dd;
   const [bills, setBills] = useState();
+  const [date, setDate] = useState(today);
+
   useEffect(() => {
-    Axios.get('/bills.json?orderBy="date"&limitToLast=100').then((res) => {
+    Axios.get(`/bills.json?orderBy="date"&equalTo="${date}"`).then((res) => {
       setBills(res.data);
     });
-  }, []);
+  }, [date]);
+
   return (
     <>
       <Analytics />
       <h2 className="m-3 text-center">Recent Bills</h2>
+      <div className="m-3 text-center">
+        <Form.Group controlId="formBasicDate">
+          <Form.Control
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </Form.Group>
+      </div>
       {bills
         ? Object.values(bills).map((bill, i) => (
             <Card
